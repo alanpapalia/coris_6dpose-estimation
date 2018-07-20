a3b26c8fe4264807744066979cddce9bc34bf8c5
Alan Papalia <alanpapalia@gmail.com>
2018-07-20 14:32:37 -0700

    attempting improved bg subtractiona and then img seg

--- bgSubTesting.py
+++ bgSubTesting.py
@@ -1,11 +1,54 @@
 import image_controls as imControl
+import cv2
+import os
+from watershed import Watershed
+from PIL import Image
+import matplotlib.pyplot as plt
+
+
+""" Takes directory of gray frames and masks
+them such that mostly black and mostly white
+pixels are made to all black
+"""
+def maskAllGrayInDir(grayFrameDir):
+    for f in os.listdir(grayFrameDir):
+    	print("Working on: " + f)
+        img = cv2.imread(grayFrameDir+f,0)
+        img = imControl.simpleThreshMaskBlkAndWhite(img)
+        # img = imControl.maskSmallWhtSquares(img)
+        img = watershedGrayImg(img)
+        cv2.imwrite(grayFrameDir+f, img)
+
+
+""" Attempt a watershed segmentation
+of color images
+"""
+
+def watershedGrayImg(gImg):
+	w = Watershed()
+	labels = w.apply(gImg)
+	return labels
 
 baseDir = './frames/single_camera/color/'
-tDir = raw_input("Which trial would you like to test? (t1/t2/t3/t4)")
-frameDir = baseDir+tDir
+tDir = raw_input("Which trial would you like to test? (t1/t2/t3/t4)\n")
+frameDir = baseDir+tDir+'/frameData/'
+grayFrameDir = baseDir+tDir+'/grayFrames/'
+
+imControl.makeGrayFrames(frameDir)
+
+maskAllGrayInDir(grayFrameDir)
+
+# frames = imControl.getFrameList(frameDir)
+# bgSubs = imControl.runBGSubMOG2(frames, 0.5)
+
+# cv2.namedWindow('original')
+# cv2.namedWindow('bg subtracted')
+# for x in xrange(len(frames)):
+#     cv2.imshow('original',frames[x])
+#     cv2.imshow('bg subtracted', bgSubs[x])
 
 
-frames = imControl.getFrameList(frameDir)
-imControl.runBGSubMOG2(frames)
 
-return
+
+
+
--- image_controls.py
+++ image_controls.py
@@ -9,6 +9,7 @@ import PIL
 from PIL import Image
 from numpy import *
 from matplotlib import pyplot as plt
+from skimage.measure import compare_ssim
 
 depth_folder = './frames/depth/'
 color_folder = './frames/color/'
@@ -169,12 +170,13 @@ order """
 
 
 def getFrameList(frameDir):
-    frames = ()
+    frames = []
 
     for f in os.listdir(frameDir):
         try:
-            if os.path.isfile(file_path):
-                frames.append(cv2.imread(frameDir+f))
+            frame = cv2.imread(frameDir+f)
+            # cv2.imshow('getList', frame)
+            frames.append(frame)
         except Exception as e:
             print(e)
 
@@ -184,24 +186,174 @@ def getFrameList(frameDir):
 """ Runs background subtraction on images """
 
 
-def runBGSubMOG2(frames):
+def runBGSubMOG2(frames, transition_threshold):
+
     fgbg = cv2.createBackgroundSubtractorMOG2()
+    prev = frames[0]
+    fgmask = fgbg.apply(prev)
     cv2.namedWindow('original')
     cv2.namedWindow('bg subtracted')
+    transitions = []
 
-    while True:
-        for f in frames:
-            # show original image
-            cv2.imshow('original', f)
+    for (i, next) in enumerate(frames[1:]):
+        prev_gray = cv2.cvtColor(prev, cv2.COLOR_BGR2GRAY)
+        next_gray = cv2.cvtColor(next, cv2.COLOR_BGR2GRAY)
+        similarity_metric = compare_ssim(prev_gray, next_gray)
+        # print('prev/next similarity measure = %f' % similarity_metric)
+        # if similarity_metric < transition_threshold:
+        fgmask = fgbg.apply(next)
+        fgdn = denoise_foreground(next, fgmask)
+        transitions.append((1, fgdn))
+        # else:
+        #     fgmask = fgbg.apply(next)
+        #     transitions.append((0, None))
+        prev = next.copy()
+    return transitions
 
-            # perform bg subtraction
-            fgmask = fgbg.apply(f)
 
-            # show bg subtracted image
-            cv2.imshow('bg subtracted', fgmask)
+"""Method pulled from online
 
-            # wait for user to press 'q' to end
-            k = cv2.waitKey(10) & 0xff
-            keyPress = cv2.waitKey(1) & 0xFF
-            if keyPress == ord('q'):
-                return
+[description]
+"""
+
+
+def denoise_foreground(img, fgmask):
+    img_bw = 255*(fgmask > 5).astype('uint8')
+    se1 = cv2.getStructuringElement(cv2.MORPH_RECT, (5, 5))
+    se2 = cv2.getStructuringElement(cv2.MORPH_RECT, (2, 2))
+    mask = cv2.morphologyEx(img_bw, cv2.MORPH_CLOSE, se1)
+    mask = cv2.morphologyEx(mask, cv2.MORPH_OPEN, se2)
+    mask = np.dstack([mask, mask, mask]) / 255
+    img_dn = img * mask
+    return img_dn
+
+
+""" Make a new directory given filepath
+
+[description]
+"""
+
+
+def makeFolder(direct):
+    try:
+        if not os.path.exists(direct):
+            os.makedirs(direct)
+    except OSError:
+        print("Cannot make directory: " + direct)
+
+
+""" Takes a given directory containing RGB frames
+and writes grayscale versions to directory sharing same parent
+directory
+"""
+
+
+def makeGrayFrames(frameDir):
+    grayDir = frameDir + "../grayFrames/"
+    makeFolder(grayDir)
+    for f in os.listdir(frameDir):
+        try:
+            colFrame = cv2.imread(frameDir+f)
+            grayFrame = cv2.cvtColor(colFrame, cv2.COLOR_RGB2GRAY)
+            cv2.imwrite(grayDir+f, grayFrame)
+        except Exception as e:
+            print(e)
+
+
+"""Takes grayscale image and converts it
+such that mostly black and mostly white
+pixels are made to all black based on
+simple thresholding of highest and lowest vals
+"""
+
+
+def simpleThreshMaskBlkAndWhite(grayImg):
+    img = grayImg.copy()
+    ret, img = cv2.threshold(img, 40, 255, cv2.THRESH_TOZERO)
+    ret, img = cv2.threshold(img, 200, 255, cv2.THRESH_TOZERO_INV)
+    return img
+
+
+"""Takes grayscale images and masks all black
+and white squares from checkerboard based on
+size (num pixels) and distance. Works assuming
+that squares are aligned with image orientation
+"""
+
+
+def maskSmallWhtSquares(grayImg):
+    row, col = grayImg.shape
+    markers = np.zeros((row, col))
+
+    cnt = 1
+
+    # vars to track size of square to mark sizes
+    sqrWid = 0
+    sqrHt = 0
+    r = 0
+    c = 0
+    # iterate through all pixels in image
+    while r < row:
+        while c < col:
+            sqrWid = 0
+            sqrHt = 0
+            if markers[r][c] == 0 and grayImg[r][c] > 10:
+                # iterate through pixels to the right and down
+                # until markedly different grayvals
+                while abs(grayImg[r][c]-grayImg[r+sqrWid][c]) < 50:
+                    if r+sqrWid == 479:
+                        break
+                    else:
+                        sqrWid += 1
+                while abs(grayImg[r][c]-grayImg[r][c+sqrHt]) < 50:
+                    if c+sqrHt == 639:
+                        break
+                    else:
+                        sqrHt += 1
+                # if small enough area and approx square, black
+                # out the object
+                print("size: " + str(sqrWid*sqrHt))
+                if sqrWid*sqrHt > 15 and sqrWid*sqrHt < 250 and abs(sqrHt-sqrWid < 100):
+                    for x in xrange(sqrWid):
+                        for y in xrange(sqrHt):
+                            markers[r+x][c+y] = 1
+                            grayImg[r+x][c+y] = 0
+                print(cnt)
+                cnt += 1
+            r += sqrWid
+            c += 1
+        r += 1
+        c = 0
+    return grayImg
+
+
+def watershedSeg(img):
+    gray = cv2.cvtColor(img,cv2.COLOR_BGR2GRAY)
+    ret, thresh = cv2.threshold(gray,0,255,cv2.THRESH_BINARY_INV+cv2.THRESH_OTSU)
+    # noise removal
+    kernel = np.ones((3, 3), np.uint8)
+    opening = cv2.morphologyEx(thresh, cv2.MORPH_OPEN, kernel, iterations=2)
+
+    # sure background area
+    sure_bg = cv2.dilate(opening, kernel, iterations=3)
+
+    # Finding sure foreground area
+    dist_transform = cv2.distanceTransform(opening, cv2.DIST_L2, 5)
+    ret, sure_fg = cv2.threshold(
+        dist_transform, 0.7*dist_transform.max(), 255, 0)
+
+    # Finding unknown region
+    sure_fg = np.uint8(sure_fg)
+    unknown = cv2.subtract(sure_bg, sure_fg)
+
+    # Marker labelling
+    ret, markers = cv2.connectedComponents(sure_fg)
+
+    # Add one to all labels so that sure background is not 0, but 1
+    markers = markers+1
+
+    # Now, mark the region of unknown with zero
+    markers[unknown == 255] = 0
+
+    markers = cv2.watershed(img, markers)
+    img[markers == -1] = [255, 0, 0]
