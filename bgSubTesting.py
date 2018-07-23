6735ff60ab1a667e32e2b425a092bcc484e9f0a0
Alan Papalia <alanpapalia@gmail.com>
2018-07-23 12:01:49 -0700

    commit

--- bgSubTesting.py
+++ bgSubTesting.py
@@ -10,24 +10,49 @@ import matplotlib.pyplot as plt
 them such that mostly black and mostly white
 pixels are made to all black
 """
+
+
 def maskAllGrayInDir(grayFrameDir):
     for f in os.listdir(grayFrameDir):
-    	print("Working on: " + f)
-        img = cv2.imread(grayFrameDir+f,0)
+        print("Working on: " + f)
+        img = cv2.imread(grayFrameDir+f, 0)
         img = imControl.simpleThreshMaskBlkAndWhite(img)
         # img = imControl.maskSmallWhtSquares(img)
         img = watershedGrayImg(img)
         cv2.imwrite(grayFrameDir+f, img)
 
 
+def cannyDetectAllGrayInDir(grayFrameDir):
+    for f in os.listdir(grayFrameDir):
+        print("Working on: " + f)
+        img = cv2.imread(grayFrameDir+f, 0)
+        # ret, img = cv2.threshold(img, 20, 255, cv2.THRESH_TOZERO)
+        # ret, img = cv2.threshold(img, 220, 255, cv2.THRESH_TOZERO_INV)
+        img = cv2.Canny(img, 70, 120)
+
+        # img = imControl.maskSmallWhtSquares(img)
+        cv2.imwrite(grayFrameDir+f, img)
+
+def findCornersAllGrayInDir(grayFrameDir):
+    for f in os.listdir(grayFrameDir):
+        print("Working on: " + f)
+        img = cv2.imread(grayFrameDir+f, 0)
+        ret, corners = cv2.findChessboardCorners(img, (10, 9), None)
+        print corners
+
+        # img = imControl.maskSmallWhtSquares(img)
+        cv2.imwrite(grayFrameDir+f, img)
+
 """ Attempt a watershed segmentation
 of color images
 """
 
+
 def watershedGrayImg(gImg):
-	w = Watershed()
-	labels = w.apply(gImg)
-	return labels
+    w = Watershed()
+    labels = w.apply(gImg)
+    return labels
+
 
 baseDir = './frames/single_camera/color/'
 tDir = raw_input("Which trial would you like to test? (t1/t2/t3/t4)\n")
@@ -36,8 +61,8 @@ grayFrameDir = baseDir+tDir+'/grayFrames/'
 
 imControl.makeGrayFrames(frameDir)
 
-maskAllGrayInDir(grayFrameDir)
-
+# cannyDetectAllGrayInDir(grayFrameDir)
+findCornersAllGrayInDir(grayFrameDir)
 # frames = imControl.getFrameList(frameDir)
 # bgSubs = imControl.runBGSubMOG2(frames, 0.5)
 
@@ -46,9 +71,3 @@ maskAllGrayInDir(grayFrameDir)
 # for x in xrange(len(frames)):
 #     cv2.imshow('original',frames[x])
 #     cv2.imshow('bg subtracted', bgSubs[x])
-
-
-
-
-
-
